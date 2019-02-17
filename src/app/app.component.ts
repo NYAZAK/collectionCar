a58e5536bf39a63b3f940b962929aeacc433b963
NYA Zakaria <zakaria.nya@hetic.net>
2019-02-17 11:05:11 +0100

    [FEAT] Ajoute l'affichage de la liste de voitures

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Car } from './shared/car.model';
+import {CarService} from './services/car.service';
+// import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'collectionCar';
+export class AppComponent implements OnInit {
+  cars: Car[];
+  error: '';
+  success = '';
+  constructor(private carS: CarService){}
+
+ngOnInit(){
+  this.getCars();
+}
+
+getCars(): void {
+  this.carS.getAll().subscribe(
+    (res: Car[]) => {
+      this.cars = res;
+    },
+    (err) => {
+      this.error = err;
+    }
+  )
+}
+
 }
